4562c9e88eeb3666bc08fb4754324ce2ff26eff2
Alexis Ferrere <alexferrere@outlook.fr>
2025-01-12 17:37:26 +0100

    Initial commit

---
+++ rails-api/config/routes.rb
@@ -0,0 +1,11 @@
+rails-api
+├── app
+│   ├── controllers
+│   │   ├── application_controller.rb
+│   │   └── api
+│   │       └── v1
+├── config
+│   ├── routes.rb
+│   └── application.rb
+├── Gemfile
+└── README.md
